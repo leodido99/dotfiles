0215db5d2ddd83cb17d7d7d9d402ef761cad8440
Léonard Bise <leonard.bise@geo-satis.com>
2018-11-19 14:41:22 +0100

    Make scripts a hidden folder

--- install.sh
+++ install.sh
@@ -36,8 +36,8 @@ rm -f $HOME/.vimrc
 ln -s $PWD/vim/.vimrc $HOME/.vimrc
 rm -f $HOME/.tmux.conf
 ln -s $PWD/tmux/.tmux.conf $HOME/.tmux.conf
-rm -rf $HOME/scripts
-ln -s $PWD/scripts $HOME/scripts
+rm -rf $HOME/.scripts
+ln -s $PWD/scripts $HOME/.scripts
 
 # Setup vim
 if [ ! -d $HOME/.vim/autoload ]; then
