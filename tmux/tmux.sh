79a6b92a67f2e4bf02e4a5d19f0461a1371f1c81
Léonard Bise <leonard.bise@gmail.com>
2019-02-01 13:22:05 +0100

    Remove unneeded tmux plugin installation

--- tmux/tmux.sh
+++ tmux/tmux.sh
@@ -7,9 +7,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
 # Install tmux plugin manager
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
-# Install tmux nord theme
-git clone https://github.com/arcticicestudio/nord-tmux ~/.tmux/themes/nord-tmux
-
 # Setup symlinks
 NAME=".tmux.conf"
 DSTDIR="$HOME"
