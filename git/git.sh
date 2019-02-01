983c90c56c04ab1c13a22b2ae4f4b7ded4314e5d
Léonard Bise <leonard.bise@gmail.com>
2019-02-01 11:02:25 +0100

    Add git hook

---
+++ git/git.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/bash
+# Setup git
+
+# Get script's dir
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
+
+# Setup symlinks
+NAME=".gitconfig"
+DSTDIR="$HOME"
+SRCDIR="$DIR"
+if [ ! -L $DSTDIR/$NAME ]; then
+	echo "Setting $DSTDIR/$NAME symlink"
+	rm -rf $DSTDIR/$NAME
+	ln -s $SRCDIR/$NAME $DSTDIR/$NAME
+else
+	echo "$DSTDIR/$NAME symlink already set"
+fi
+
+NAME=".gitignore"
+DSTDIR="$HOME"
+SRCDIR="$DIR"
+if [ ! -L $DSTDIR/$NAME ]; then
+	echo "Setting $DSTDIR/$NAME symlink"
+	rm -rf $DSTDIR/$NAME
+	ln -s $SRCDIR/$NAME $DSTDIR/$NAME
+else
+	echo "$DSTDIR/$NAME symlink already set"
+fi
+
+mkdir -p $HOME/.git/hooks
+NAME="commit-msg"
+DSTDIR="$HOME/.git/hooks"
+SRCDIR="$DIR"
+if [ ! -L $DSTDIR/$NAME ]; then
+	echo "Setting $DSTDIR/$NAME symlink"
+	rm -rf $DSTDIR/$NAME
+	ln -s $SRCDIR/$NAME $DSTDIR/$NAME
+else
+	echo "$DSTDIR/$NAME symlink already set"
+fi
