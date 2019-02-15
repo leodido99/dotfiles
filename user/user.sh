56b1a2316d83f6b381478a7b10dfb3ab0d1aa0b5
Léonard Bise <leonard.bise@gmail.com>
2019-02-15 09:38:55 +0100

    Add user setup script

---
+++ user/user.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Setup user
+
+# Get script's dir
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
+
+if [ $1 = "Ubuntu" ] || [ $1 = "Fedora" ]; then
+	# Access tty dev
+	usermod -a -G dialout $USER
+fi
