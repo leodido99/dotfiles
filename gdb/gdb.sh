75be08844e89a921336a7b805d3bf327e84ecdeb
Léonard Bise <leonard.bise@gmail.com>
2019-08-26 12:11:10 +0200

    Add gdb script

---
+++ gdb/gdb.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Get script's dir
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
+
+# TODO
+
+# Setup symlinks
+#SRC="$DIR/.gdbinit"
+#DST="$HOME/.scripts"
+#$DIR/../tools/sym_link.sh $SRC $DST
+
+
