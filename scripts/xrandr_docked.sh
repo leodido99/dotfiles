42cb0e75e9c5f8df9235ea01f3afa17a2b5c2b21
Léonard Bise <leonard.bise@gmail.com>
2019-10-01 10:47:20 +0200

    scripts: Add xrandr scripts

---
+++ scripts/xrandr_docked.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+xrandr --output eDP-1 --mode 1920x1080 --pos 3840x0 --crtc 1 --output HDMI-3 --off --output HDMI-2 --off --output HDMI-1 --off --output DP-3-1 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --crtc 0 --output DP-3-3 --off --output DP-3-2 --mode 1920x1080 --pos 0x0 --rotate normal --crtc 2 --output DP-3 --off --output DP-2 --off --output DP-1 --off
---
+++ scripts/xrandr_laptop.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+xrandr --output eDP-1 --mode 1920x1080 --pos 3840x0 --crtc 1 --output HDMI-3 --off --output HDMI-2 --off --output HDMI-1 --off --output DP-3-1 --off --output DP-3-3 --off --output DP-3-2 --off --output DP-3 --off --output DP-2 --off --output DP-1 --off
