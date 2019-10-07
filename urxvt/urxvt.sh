6dbe0c810ddfc682be743fd8dfb8a876c55d6d64
Léonard Bise <leonard.bise@gmail.com>
2019-10-07 07:44:08 +0200

    Fix urxvt install and usage

--- urxvt/urxvt.sh
+++ urxvt/urxvt.sh
@@ -7,14 +7,14 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
 DSTDIR="$HOME/.fonts"
 
 # Have to use multilangue rxvt otherwise unicode3 not compiled in and power line don't work
-sudo dnf install rxvt-unicode-ml
+sudo dnf install rxvt-unicode-256color-ml
 
 # Clone nord theme
 git clone https://github.com/arcticicestudio/nord-xresources.git $HOME/gitrepo/nord-xresources
 
 # Get font
 cd $DSTDIR
- curl -fLo "Sauce Code Pro Nerd Font Complete Mono.otf" https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/SourceCodePro/Regular/complete/Sauce%20Code%20Pro%20Nerd%20Font%20Complete%20Mono.ttf
+curl -fLo "Sauce Code Pro Nerd Font Complete Mono.ttf" "https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/SourceCodePro/Regular/complete/Sauce Code Pro Nerd Font Complete Mono.ttf"
 
 fc-cache -fv $DSTDIR
 
