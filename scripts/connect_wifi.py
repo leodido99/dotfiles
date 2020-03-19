9abcb7b75674e2f137d4bef30bac5a1334838a3b
Léonard Bise <leonard.bise@gmail.com>
2020-03-19 08:53:58 +0100

    scripts: Add connect wifi script

---
+++ scripts/connect_wifi.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+import argparse
+import subprocess
+import sys
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,
+            description='Connect to wifi')
+    parser.add_argument('command',
+                    help = 'Command to run')
+
+    parser.add_argument('-s', '--ssid',
+                    help = 'SSID to connect to')
+    args = parser.parse_args()
+
+    if args.command == 'list':
+        ret = subprocess.call(['nmcli', 'device', 'wifi', 'list', '--rescan' , 'yes'])
+        if ret != 0:
+            raise RuntimeError('Cannot list wifi')
+    elif args.command == 'connect':
+        if args.ssid is None:
+            print('SSID missing')
+            sys.exit()
+        ret = subprocess.call(['nmcli', 'device', 'wifi', 'connect', args.ssid, '--ask'])
+        if ret != 0:
+            raise RuntimeError('Cannot connect wifi')
+
