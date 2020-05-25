18340d0b725f7a3b8e539814c129815ce962b813
Léonard Bise <leonard.bise@gmail.com>
2020-05-25 14:51:13 +0200

    Add ctags file

---
+++ ctags/ctags.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# Setup ctags
+# $1: OS
+
+# Get script's dir
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
+
+source $DIR/../tools/pkg_mgr.sh
+
+sudo $INSTALLCMD ctags
+
+# Setup symlinks
+NAME=".ctags"
+SRC="$DIR/$NAME"
+DST="$HOME/$NAME"
+$DIR/../tools/sym_link.sh $SRC $DST
