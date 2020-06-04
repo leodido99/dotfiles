2c172902e4f44c9cf8aaa42c8ff01171cae1217f
Léonard Bise <leonard.bise@gmail.com>
2020-06-04 07:32:48 +0200

    scripts: Add xrandr home script

---
+++ scripts/xrandr_home.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+xrandr --output DVI-I-1 --mode 1920x1080 --pos 2560x0 --rotate normal --output DVI-D-1 --off --output HDMI-1 --off --output DP-1 --mode 2560x1440 --pos 0x0 --rotate normal
