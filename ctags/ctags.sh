5bdfcadb99a42ff9d1eae87974b181c73ffe21b6
Léonard Bise <leonard.bise@gmail.com>
2020-06-25 15:43:53 +0200

    ctags: Add config file for universal ctags

--- ctags/ctags.sh
+++ ctags/ctags.sh
@@ -10,7 +10,14 @@ source $DIR/../tools/pkg_mgr.sh
 sudo $INSTALLCMD ctags
 
 # Setup symlinks
+# Exuberant ctags
 NAME=".ctags"
 SRC="$DIR/$NAME"
 DST="$HOME/$NAME"
+$DIR/../tools/sym_link.sh $SRC $DST
+
+# Universal ctags
+NAME=".ctags"
+SRC="$DIR/$NAME"
+DST="$HOME/.ctags.d/default$NAME"
 $DIR/../tools/sym_link.sh $SRC $DST
