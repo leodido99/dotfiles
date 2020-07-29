33fa8e1add249410ba7a537e47e12a82b64f27df
Léonard Bise <leonard.bise@gmail.com>
2020-07-29 10:21:22 +0200

    scripts: Add FES gp config script

---
+++ scripts/geo_fes_gp_cfg.sh
@@ -0,0 +1,4 @@
+export FES_DOWNLOAD="gstfes:9091"
+export FES_DOWNLOAD_CERT="$HOME/go/src/rages/certs/gstfes_download_cert.pem"
+export FES_CLIENT_CERT="$HOME/go/src/rages/certs/tls_cert.pem"
+export FES_CLIENT_KEY="$HOME/go/src/rages/certs/tls_key.pem"
